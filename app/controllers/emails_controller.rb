500169b634a7588796edf679cfc32b67bb6bfe27
Tonie <toniemontfort@gmail.com>
2019-08-30 19:34:42 +0300

    modification

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -6,7 +6,7 @@ class EmailsController < ApplicationController
 	def create
 		@email = Email.new(object:Faker::Hipster.sentence(word_count: 2),
 		body:Faker::Hipster.paragraph(sentence_count: 2,
-			supplemental: true, random_sentences_to_add: 4))
+			supplemental: true, random_sentences_to_add: 4), read: false)
 			@email.save
 		respond_to do |format|
 			format.html{
@@ -20,7 +20,7 @@ class EmailsController < ApplicationController
 	end
 
 	def show
-		@email = Email.find(params[:id])
+		@email = find_email
 		respond_to do |format|
 			format.html{}
 			format.js{}
@@ -28,9 +28,33 @@ class EmailsController < ApplicationController
 
 
 	end
-
-	private
 	def destroy
+		@email= find_email
+		@email.destroy
+		respond_to do |format|
+			format.html{
+			redirect_to emails_path, success: "Deleted successfull"
+			}
+			format.js{}
+		end
+		
+	end
 
+	def update
+		@email = find_email
+		if @email.read === false
+			@email.update(read: true)
+		else
+			@email.update(read: false)
+		end
+
+		respond_to do |format|
+				format.html{}
+				format.js{}
+		end
+	end
+	private
+	def find_email
+		Email.find(params[:id])
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :tasks, except: [:show]
-  resources :emails, only: [:index, :create, :show]
+  resources :emails, except: [:edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
